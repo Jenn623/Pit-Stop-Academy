960d21648d0d4d86ce6b387095fad2340232a680
Jenn <akejenn@gmail.com>
2026-02-12 22:52:42 -0600

    Actualizacion en el carrusel de imagenes

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -6,17 +6,22 @@ function Layout ({children}){
     return(
 
         <>
-            <div>
+        {/* d-flex: activa el flexbox */}
+        {/* flex-column: organiza a los componentes hijos en columna vertical */}
+        {/* min-vh-100: asegura que el contenedor mida al menos el 100% de alto de la ventana */}
+            <div className="d-flex flex-column min-vh-100">
                 {/*El header siempre estará arriba */}
                 <Header></Header>
 
                 {/*En el Main Section va a estar los componentes de la página a renderizar */}
-                <main>
+                {/* flex-grow-1 hace que la seccion main ocupe el resto del espacio disponible */}
+                <main className="flex-grow-1">
                     {children}
                 </main>
 
                 {/*El footer siempre estará en la parte inferior */}
-                <FooterComp></FooterComp>
+                {/* mt-auto provoca un margen top automatico */}
+                <FooterComp className="mt-auto"></FooterComp>
             </div>
         </>
     )
--- src/Pages/Teams&Racers.jsx
+++ src/Pages/Teams&Racers.jsx
@@ -6,10 +6,15 @@ function TeamsRacers(){
     return (
 
         <>
-            <MainSection title="¿Quién es Quién en la Pista?"
+        <div className="pt-4 mt-5">
+            <MainSection
+            title="¿Quién es Quién en la Pista?"
             description="Te presento a la Parrila. Aquí es donde se arman los equipos. Hay 11 escuderías y cada una tiene 2 pilotos estrela. ¡Elige a tu starter!">
             </MainSection>
+        </div>
+        <div className="pt-5">
             <TeamCarousel></TeamCarousel>
+        </div>
         </>
     )
 }
